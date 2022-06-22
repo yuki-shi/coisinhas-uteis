efce0c85b3e682c968e9e1241a58a898ecbfc074
yuki-shi <88805836+yuki-shi@users.noreply.github.com>
2022-06-22 20:18:48 -0300

    Comparar KWs com API do Google Trends

---
+++ trends_comparasion.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pytrends.request import TrendReq
+import datetime as dt
+
+def trends_table(kw_list): 
+  pytrends = TrendReq(hl='pt-BR', tz=180)
+
+  for i in kw_list:
+    pytrends.build_payload(kw_list=[f'{i}'], geo='BR', timeframe='today 12-m')
+    if df.empty:
+      df = pytrends.interest_over_time()
+    else:
+      df[f'{i}'] = pytrends.interest_over_time().iloc[:,0]
+
+  df = df.reset_index(level=0)
+  df.drop('isPartial', axis=1, inplace=True)
+
+  df['date-str'] = df['date'].dt.strftime('%B/%Y')
+  df_final = df.groupby('date-str').max().reset_index().sort_values('date')
+  df_final.drop('date', axis=1, inplace=True)
