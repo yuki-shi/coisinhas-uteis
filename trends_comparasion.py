2ca190d4523ad95ab20dc9967afd33fe7e834c9b
yuki-shi <88805836+yuki-shi@users.noreply.github.com>
2022-07-04 16:29:40 -0300

    Update trends_comparasion.py

--- trends_comparasion.py
+++ trends_comparasion.py
@@ -3,7 +3,14 @@ from pytrends.request import TrendReq
 import datetime as dt
 
 #--------------------------
-  # kw_list = Lista de KWs a serem buscadas no Trends
+   # Criar dataframe vazio e preenchê-lo consome mais memória do que
+   # realizar a operação com listas, o ideal seria transformar o dataframe
+   # resultante de .interest_over_time() em dict e, então, concatenar
+   # a lista de dicts em um novo dataframe.
+   # Por ora, como para meus fins a iteraço não ocorre mais que 5 vezes,
+   # creio que esteja ok.
+
+   # kw_list = lista de KWs a serem buscadas no Google Trends
 #--------------------------
 
 def trends_table(kw_list): 
@@ -22,3 +29,5 @@ def trends_table(kw_list):
   df['date-str'] = df['date'].dt.strftime('%B/%Y')
   df_final = df.groupby('date-str').max().reset_index().sort_values('date')
   df_final.drop('date', axis=1, inplace=True)
+  
+  return df_final
